d2715ac2917d9f6084b46d7c4bd4a4cd4034c41e
Alberto Duarte <betodute@gmail.com>
2019-04-10 17:21:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { BillieHoliday : "Sophisticated Lady" };
 
 function updatePlaylist (playlist, key, value) {
-  playlist[key] = value;
+  playlist.key = value;
   return playlist;
 }
